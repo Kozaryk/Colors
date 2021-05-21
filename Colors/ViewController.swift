1d88b696755c7bbb82444906e5c62bdb292d055b
Igor Kozar <kozaryk99@gmail.com>
2021-05-21 23:39:08 +0300

    Finish

--- Colors/ViewController.swift
+++ Colors/ViewController.swift
@@ -19,9 +19,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSliderValue: UITextField!
     @IBOutlet weak var greenSliderValue: UITextField!
     @IBOutlet weak var blueSliderValue: UITextField!
+    @IBOutlet weak var resetButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mixView.layer.cornerRadius = 10
+        mixView.layer.borderWidth = 2
+        
+        mixView.layer.shadowOffset = CGSize(width: 5, height: 5)
+        mixView.layer.shadowOpacity = 0.4
+        mixView.layer.shadowRadius = 5
+        
+        resetButton.layer.shadowOffset = CGSize(width: 5, height: 5)
+        resetButton.layer.shadowOpacity = 0.4
+        resetButton.layer.shadowRadius = 5
+        
+        shadows()
+
     }
     
 
@@ -42,12 +57,39 @@ class ViewController: UIViewController {
         colorMixer()
     }
     
+    @IBAction func resetColor(_ sender: Any) {
+        reset()
+    }
+    
+    
     func colorMixer() {
-        self.mixView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                                    green: CGFloat(greenSlider.value),
-                                                    blue: CGFloat(blueSlider.value),
-                                                    alpha: CGFloat(transparencySlider.value))
-        
+        self.mixView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: CGFloat(transparencySlider.value))
+    }
+    
+    func shadows() {
+        let array = [redSlider, greenSlider, blueSlider,
+                     transparencySlider, redSliderValue,
+                     greenSliderValue, blueSliderValue]
+        for index in array{
+            index?.layer.shadowOffset = CGSize(width: 2, height: 2)
+            index?.layer.shadowOpacity = 0.4
+            index?.layer.shadowRadius = 5
+        }
+    }
+    
+    func reset(){
+        redSlider.value = 0
+        greenSlider.value = 0
+        blueSlider.value = 0
+        redSliderValue.text = String("")
+        greenSliderValue.text = String("")
+        blueSliderValue.text = String("")
+        transparencySlider.value = 1
+        mixView.backgroundColor = UIColor.white
     }
     
 }
