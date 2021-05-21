9dbb37d86339fd7d0834c1741f81916f51e17516
Igor Kozar <kozaryk99@gmail.com>
2021-05-21 20:20:21 +0300

    Complete mix

--- Colors/ViewController.swift
+++ Colors/ViewController.swift
@@ -11,15 +11,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    
     @IBOutlet weak var transparencySlider: UISlider!
     
     @IBOutlet weak var mixView: UIView!
+   
+    @IBOutlet weak var redSliderValue: UITextField!
+    @IBOutlet weak var greenSliderValue: UITextField!
+    @IBOutlet weak var blueSliderValue: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func redSliderChangeValue(_ sender: Any) {
+        redSliderValue.text = String(format: "%.2f", redSlider.value )
+        colorMixer()
+    }
+    @IBAction func greenSliderChangeValue(_ sender: Any) {
+        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
+        colorMixer()
+    }
+    @IBAction func blueSliderChangeValue(_ sender: Any) {
+        blueSliderValue.text = String(format: "%.2f", blueSlider.value)
+        colorMixer()
+    }
+    
+    @IBAction func transparencyChangeValue(_ sender: Any) {
+        colorMixer()
+    }
+    
+    func colorMixer() {
+        self.mixView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                                    green: CGFloat(greenSlider.value),
+                                                    blue: CGFloat(blueSlider.value),
+                                                    alpha: CGFloat(transparencySlider.value))
+        
+    }
+    
 }
 
