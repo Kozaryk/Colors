10cd155ba4fcc3dd9113a67714348346efff587c
Igor Kozar <kozaryk99@gmail.com>
2021-05-21 18:18:01 +0300

    Edit Layout

--- Colors/ViewController.swift
+++ Colors/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var transparencySlider: UISlider!
+    
+    @IBOutlet weak var mixView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
